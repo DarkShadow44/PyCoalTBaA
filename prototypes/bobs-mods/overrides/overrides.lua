26a72d46fef401f311e4e176222ca0ec7fe54bca
Fabian Maurer <dark.shadow4@web.de>
2020-06-04 23:33:14 +0200

    Fix crash when Angel's refining is missing

--- prototypes/bobs-mods/overrides/overrides.lua
+++ prototypes/bobs-mods/overrides/overrides.lua
@@ -340,48 +340,32 @@ if mods['pyhightech'] then
     RECIPE('wood-board'):set_enabled(false)
 end
 
+function remove_ore(ore)
+	data.raw.resource[ore] = nil
+	data.raw["autoplace-control"][ore] = nil
+	for _, preset in pairs(data.raw["map-gen-presets"]["default"]) do
+		if preset.basic_settings ~= nil and preset.basic_settings.autoplace_controls ~= nil then
+			preset.basic_settings.autoplace_controls[ore] = nil
+		end
+	end
+end
+
 --disable bobs ore when pyro is active
 if mods['pyrawores'] then
     if mods['bobores'] then
-        --log(serpent.block(data.raw["autoplace-control"]))
-        data.raw.resource['bauxite-ore'] = nil
-        data.raw['autoplace-control']['bauxite-ore'] = nil
-
-        data.raw.resource['cobalt-ore'] = nil
-        data.raw['autoplace-control']['cobalt-ore'] = nil
-
-        data.raw.resource['gem-ore'] = nil
-        data.raw['autoplace-control']['gem-ore'] = nil
-
-        data.raw.resource['gold-ore'] = nil
-        data.raw['autoplace-control']['gold-ore'] = nil
-
-        data.raw.resource['lead-ore'] = nil
-        data.raw['autoplace-control']['lead-ore'] = nil
-
-        data.raw.resource['nickel-ore'] = nil
-        data.raw['autoplace-control']['nickel-ore'] = nil
-
-        data.raw.resource['quartz'] = nil
-        data.raw['autoplace-control']['quartz'] = nil
-
-        data.raw.resource['rutile-ore'] = nil
-        data.raw['autoplace-control']['rutile-ore'] = nil
-
-        data.raw.resource['silver-ore'] = nil
-        data.raw['autoplace-control']['silver-ore'] = nil
-
-        data.raw.resource['sulfur'] = nil
-        data.raw['autoplace-control']['sulfur'] = nil
-
-        data.raw.resource['tin-ore'] = nil
-        data.raw['autoplace-control']['tin-ore'] = nil
-
-        data.raw.resource['tungsten-ore'] = nil
-        data.raw['autoplace-control']['tungsten-ore'] = nil
-
-        data.raw.resource['zinc-ore'] = nil
-        data.raw['autoplace-control']['zinc-ore'] = nil
+        remove_ore('bauxite-ore')
+        remove_ore('cobalt-ore')
+        remove_ore('gem-ore')
+        remove_ore('gold-ore')
+        remove_ore('lead-ore')
+        remove_ore('nickel-ore')
+        remove_ore('quartz')
+        remove_ore('rutile-ore')
+        remove_ore('silver-ore')
+        remove_ore('sulfur')
+        remove_ore('tin-ore')
+        remove_ore('tungsten-ore')
+        remove_ore('zinc-ore')
 
     --data.raw.resource["lithia-water"] = nil
     --data.raw["autoplace-control"]["lithia-water"] = nil
