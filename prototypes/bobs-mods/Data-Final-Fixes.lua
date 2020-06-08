86674b425fe90ddb53c1e821f840ba63a1695fb7
Fabian Maurer <dark.shadow4@web.de>
2020-06-08 17:49:54 +0200

    Fix heavy-water name

--- prototypes/bobs-mods/Data-Final-Fixes.lua
+++ prototypes/bobs-mods/Data-Final-Fixes.lua
@@ -4,6 +4,6 @@ if mods["bobplates"] then
 				--log(serpent.block(data.raw.recipe["heavy-water"]))
 				data.raw.recipe["heavy-water"].icon_size=32
 				data.raw.recipe["heavy-water"].icon = "__pyfusionenergygraphics__/graphics/icons/heavy-water.png"
-				data.raw.recipe["heavy-water"].main_product="heavy-water"
+				data.raw.recipe["heavy-water"].main_product="liquid-water-heavy"
 			end
 		end
